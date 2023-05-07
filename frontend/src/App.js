9e3054cb560b65261124980c20c3440f33731305
--unset <haroonbajwa99@gmail.com>
2023-05-07 15:41:15 +0500

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" bg-slate-800 text-white">Multivendor Ecommerce Store</h1>
+    </div>
+  );
+}
+
+export default App;
